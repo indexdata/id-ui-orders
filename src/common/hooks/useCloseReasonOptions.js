1dce7d3b208cf373ab449be00621bf835634897c
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-03-17 17:54:18 +0300

    fix error after page refreshed and user returns to the orders list (#803)

--- src/common/hooks/useCloseReasonOptions.js
+++ src/common/hooks/useCloseReasonOptions.js
@@ -4,7 +4,7 @@ import { DEFAULT_CLOSE_ORDER_REASONS } from '../constants';
 
 const useCloseReasonOptions = (formatMessage, backendReasons) => {
   const translatedReasonsOptions = useMemo(
-    () => backendReasons.map(({ reason }) => {
+    () => backendReasons?.map(({ reason }) => {
       const reasonTranslationKey = DEFAULT_CLOSE_ORDER_REASONS[reason];
       const reasonTranslationValue = reasonTranslationKey
         ? formatMessage({
