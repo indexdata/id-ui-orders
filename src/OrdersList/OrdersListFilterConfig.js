72dc8db3165ba13c1aa138564b612a5167ba271f
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-07-08 12:23:47 +0300

    fix search by reason for closure (#920)

--- src/OrdersList/OrdersListFilterConfig.js
+++ src/OrdersList/OrdersListFilterConfig.js
@@ -1,4 +1,4 @@
-import { buildDateRangeQuery } from '@folio/stripes-acq-components';
+import { buildDateRangeQuery, ORDER_STATUSES } from '@folio/stripes-acq-components';
 
 import { FILTERS } from './constants';
 
@@ -6,4 +6,7 @@ export const customFilterMap = {
   [FILTERS.DATE_CREATED]: buildDateRangeQuery.bind(null, [FILTERS.DATE_CREATED]),
   [FILTERS.RENEWAL_DATE]: buildDateRangeQuery.bind(null, [FILTERS.RENEWAL_DATE]),
   [FILTERS.DATE_ORDERED]: buildDateRangeQuery.bind(null, [FILTERS.DATE_ORDERED]),
+  [FILTERS.CLOSE_REASON]: (filterValue) => {
+    return `(${FILTERS.CLOSE_REASON}=="${filterValue}" and ${FILTERS.STATUS}=="${ORDER_STATUSES.closed}")`;
+  },
 };
