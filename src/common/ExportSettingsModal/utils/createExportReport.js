dbc85e557058788ab27dd4ffa0edddde949f035c
AndreiShumski2 <Andrei_Shumski2@epam.com>
2021-01-29 14:46:59 +0300

    UIOR-645 Add more export fields (#1045)

--- src/common/ExportSettingsModal/utils/createExportReport.js
+++ src/common/ExportSettingsModal/utils/createExportReport.js
@@ -1,8 +1,8 @@
-import { getFullName } from '@folio/stripes/util';
 import {
   calculateFundAmount,
   formatDate,
   formatDateTime,
+  FUND_DISTR_TYPE,
 } from '@folio/stripes-acq-components';
 
 const getRecordMap = (records) => (
@@ -15,27 +15,32 @@ const getRecordMap = (records) => (
 
 const getContributorData = (line, contributorNameTypeMap) => (
   line.contributors?.map(({ contributor, contributorNameTypeId }) => (
-    `${contributor} - ${contributorNameTypeMap[contributorNameTypeId]?.name}`
+    `"${contributor}""${contributorNameTypeMap[contributorNameTypeId]?.name}"`
   )).join(' | ')
 );
 
 const getProductIdData = (line, identifierTypeMap) => (
   line.details?.productIds?.map(i => (
-    `${i?.productId} - ${i?.qualifier || ''} - ${identifierTypeMap[i?.productIdType]?.name}`
+    `"${i?.productId}""${i?.qualifier || ''}""${identifierTypeMap[i?.productIdType]?.name}"`
   )).join(' | ')
 );
 
 const getFundDistributionData = (line, expenseClassMap) => (
   line.fundDistribution?.map(fund => (
-    `${fund.code} - ${expenseClassMap[fund?.expenseClassId]?.name || ''} - ${fund.value} -
-    ${calculateFundAmount(fund, line.cost.poLineEstimatedPrice, line.cost.currency)}`
-  )).join(' | ')
+    `"${fund.code || ''}""${expenseClassMap[fund?.expenseClassId]?.name || ''}"
+    "${fund.value || '0'}${fund.distributionType === FUND_DISTR_TYPE.percent ? '%' : ''}"
+    "${calculateFundAmount(fund, line.cost.poLineEstimatedPrice, line.cost.currency)}"`
+  )).join(' | ').replace(/\n\s+/g, '')
 );
 
 const getLocationData = (line, locationMap) => (
   line.locations?.map(l => (
-    `${locationMap[l.locationId]?.name} - ${l?.quantityPhysical || 0} - ${l?.quantityElectronic || 0}`
+    `"${locationMap[l.locationId]?.code}""${l?.quantityPhysical || 0}""${l?.quantityElectronic || 0}"`
   )).join(' | ')
+);
+
+const getAddressData = (addressId, addressMap) => (
+  addressId ? `"${addressMap[addressId].name}""${addressMap[addressId].address}"` : ''
 );
 
 export const createExportReport = (
@@ -70,27 +75,36 @@ export const createExportReport = (
     orderType: ordersMap[lineRecord.purchaseOrderId].orderType,
     acquisitionsUnits: ordersMap[lineRecord.purchaseOrderId].acqUnitIds.map(id => acqUnitMap[id].name).join(' | '),
     approvalDate: formatDateTime(ordersMap[lineRecord.purchaseOrderId].approvalDate, intl),
-    assignedTo: getFullName(userMap[ordersMap[lineRecord.purchaseOrderId].assignedTo]),
-    billTo: addressMap[ordersMap[lineRecord.purchaseOrderId]?.billTo]?.address,
-    shipTo: addressMap[ordersMap[lineRecord.purchaseOrderId]?.shipTo]?.address,
+    assignedTo: userMap[ordersMap[lineRecord.purchaseOrderId].assignedTo]?.username,
+    billTo: getAddressData(ordersMap[lineRecord.purchaseOrderId]?.billTo, addressMap),
+    shipTo: getAddressData(ordersMap[lineRecord.purchaseOrderId]?.shipTo, addressMap),
     manualPo: ordersMap[lineRecord.purchaseOrderId].manualPo,
     reEncumber: ordersMap[lineRecord.purchaseOrderId].reEncumber,
-    createdByUserId: getFullName(userMap[ordersMap[lineRecord.purchaseOrderId].metadata?.createdByUserId]),
+    createdByUserId: userMap[ordersMap[lineRecord.purchaseOrderId].metadata?.createdByUserId]?.username,
     createdDate: formatDateTime(ordersMap[lineRecord.purchaseOrderId].metadata?.createdDate, intl),
     note: ordersMap[lineRecord.purchaseOrderId].notes?.join('|'),
     workflowStatus: ordersMap[lineRecord.purchaseOrderId].workflowStatus,
     approved: ordersMap[lineRecord.purchaseOrderId].approved,
+    interval: ordersMap[lineRecord.purchaseOrderId]?.ongoing?.interval,
+    isSubscription: ordersMap[lineRecord.purchaseOrderId]?.ongoing?.isSubscription,
+    manualRenewal: ordersMap[lineRecord.purchaseOrderId]?.ongoing?.manualRenewal,
+    ongoingNotes: ordersMap[lineRecord.purchaseOrderId]?.ongoing?.notes,
+    reviewPeriod: ordersMap[lineRecord.purchaseOrderId]?.ongoing?.reviewPeriod,
+    renewalDate: formatDate(ordersMap[lineRecord.purchaseOrderId]?.ongoing?.renewalDate, intl),
+    reviewDate: formatDate(ordersMap[lineRecord.purchaseOrderId]?.ongoing?.reviewDate, intl),
     poLineNumber: lineRecord.poLineNumber,
     titleOrPackage: lineRecord.titleOrPackage,
-    subscriptionFrom: formatDate(lineRecord.subscriptionFrom, intl),
-    subscriptionTo: formatDate(lineRecord.subscriptionTo, intl),
-    subscriptionInterval: lineRecord.subscriptionInterval,
+    instanceId: lineRecord.instanceId,
+    subscriptionFrom: formatDate(lineRecord.details?.subscriptionFrom, intl),
+    subscriptionTo: formatDate(lineRecord.details?.subscriptionTo, intl),
+    subscriptionInterval: lineRecord.details?.subscriptionInterval,
     receivingNote: lineRecord.details?.receivingNote,
     publisher: lineRecord.publisher,
     edition: lineRecord.edition,
-    packagePoLineId: poLinesMap[lineRecord.packagePoLineId]?.poLineNumber,
+    packagePoLineId: poLinesMap[lineRecord.packagePoLineId]?.poLineNumber || lineRecord.packagePoLineId,
     contributor: getContributorData(lineRecord, contributorNameTypeMap),
     productIdentifier: getProductIdData(lineRecord, identifierTypeMap),
+    description: lineRecord.description,
     acquisitionMethod: lineRecord.acquisitionMethod,
     orderFormat: lineRecord.orderFormat,
     createdDateLine: formatDate(lineRecord.metadata?.createdDate, intl),
@@ -106,23 +120,25 @@ export const createExportReport = (
     rush: lineRecord.rush,
     collection: lineRecord.collection,
     poLineDescription: lineRecord.poLineDescription,
+    refNumber: `"${lineRecord.vendorDetail?.refNumber}""${lineRecord.vendorDetail?.refNumberType}"`,
     instructions: lineRecord.vendorDetail?.instructions,
     vendorAccount: lineRecord.vendorDetail?.vendorAccount,
     listUnitPrice: lineRecord.cost?.listUnitPrice,
     quantityPhysical: lineRecord.cost?.quantityPhysical,
     listUnitPriceElectronic: lineRecord.cost?.listUnitPriceElectronic,
     quantityElectronic: lineRecord.cost?.quantityElectronic,
-    discount: lineRecord.cost?.discount,
+    discount: `"${lineRecord.cost?.discount}""${lineRecord.cost?.discountType}"`,
     poLineEstimatedPrice: lineRecord.cost.poLineEstimatedPrice,
+    currency: lineRecord.cost.currency,
     fundDistribution: getFundDistributionData(lineRecord, expenseClassMap),
     location: getLocationData(lineRecord, locationMap),
-    materialSupplier: vendorMap[lineRecord.physical?.accessProvider]?.name,
+    materialSupplier: vendorMap[lineRecord.physical?.materialSupplier]?.code,
     receiptDue: formatDate(lineRecord.physical?.receiptDue, intl),
     expectedReceiptDate: formatDate(lineRecord.physical?.expectedReceiptDate, intl),
     volumes: lineRecord.physical?.volumes.join('|'),
     createInventory: lineRecord.physical?.createInventory,
     materialType: materialTypeMap[lineRecord.physical?.materialType]?.name,
-    accessProvider: vendorMap[lineRecord.eresource?.accessProvider]?.name,
+    accessProvider: vendorMap[lineRecord.eresource?.accessProvider]?.code,
     activated: lineRecord.eresource?.activated,
     activationDue: formatDate(lineRecord.eresource?.activationDue, intl),
     createInventoryE: lineRecord.eresource?.createInventory,
